596da735a621ddcd59872b15d055be597b64e569
Daan <daanj33@gmail.com>
2020-11-29 17:41:47 +0100

    Feedback approval end points

--- controllers/FeedbackController.js
+++ controllers/FeedbackController.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import Feedback from '../models/Feedback.js';
+import User from '../models/User.js';
 import {isMgt} from '../middleware/isStaff.js';
 
 router.post('/', async (req, res) => {
@@ -26,10 +27,36 @@ router.post('/', async (req, res) => {
 	}
 });
 
+router.get('/controllers', async ({res}) => {
+	const controllers = await User.find({deletedAt: null}).sort('fname').select('fname lname _id').lean();
+	return res.json(controllers);
+});
+
 router.get('/unapproved', isMgt, async ({res}) => {
-	const feedback = await Feedback.find({deletedAt: null, approved: false}).lean();
+	const feedback = await Feedback.find({deletedAt: null, approved: false}).populate('controller', 'fname lname cid').lean();
 	return res.json(feedback);
 });
 
+router.put('/approve/:id', isMgt, async (req, res) => {
+	Feedback.findOneAndUpdate({_id: req.params.id}, {
+		approved: true
+	}).then(() => {
+		return res.sendStatus(200);
+	}).catch((err) => {
+		console.log(err);
+		return res.sendStatus(500);
+	});
+});
+
+router.put('/reject/:id', isMgt, async (req, res) => {
+	Feedback.delete({_id: req.params.id}, (err) => {
+		if(err) {
+			console.log(err);
+			return res.sendStatus(500);
+		} else {
+			return res.sendStatus(200);
+		}
+	});
+});
 
 export default router;
