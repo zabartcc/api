f3863714921cc33755a6bdddcb5fb0d6fe704aa1
Daan <daanj33@gmail.com>
2020-12-16 00:28:29 +0100

    Merged online controller end points into one.

--- controllers/OnlineController.js
+++ controllers/OnlineController.js
@@ -3,14 +3,13 @@ const router = express.Router();
 import PilotOnline from '../models/PilotOnline.js';
 import AtcOnline from '../models/AtcOnline.js';
 
-router.get('/pilots', async ({res}) => {
+router.get('/', async ({res}) => {
 	const pilots = await PilotOnline.find().lean();
-	res.json(pilots);
-});
-
-router.get('/atc', async ({res}) => {
 	const atc = await AtcOnline.find().lean({virtuals: true});
-	res.json(atc);
+	res.json({
+		pilots: pilots,
+		atc: atc
+	});
 });
 
 
