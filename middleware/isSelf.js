aaaadcc0baf45f8cfab15e97076e5bf2c79f4feb
Daan <daanj33@gmail.com>
2020-12-17 19:20:55 +0100

    Added isSelf middleware

---
+++ middleware/isSelf.js
@@ -0,0 +1,26 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
+export default function (req, res, next) {
+	if(!req.cookies.token) {
+		return res.sendStatus(401);
+	} else {
+		const userToken = req.cookies.token;
+		const userId = req.params.id;
+		jwt.verify(userToken, process.env.JWT_SECRET, async (err, decoded) => {
+			if(err) {
+				console.log(`Unable to verify token: ${err}`);
+				return res.sendStatus(401);
+			} else {
+				const user = await User.findOne({
+					cid: decoded.cid
+				}).lean();
+				if(user._id.toString() === userId) {
+					next();
+				} else {
+					return res.sendStatus(403);
+				}
+			}
+		});
+	}
+}
