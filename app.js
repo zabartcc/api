e64629c9b12a4d59016f312032e25708c9dbe254
Austin Robison <austin@arobison.net>
2021-01-04 13:07:31 -0700

    support IDS with SSE

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ import cors from 'cors';
 import env from 'dotenv';
 import mongoose from 'mongoose';
 import body from 'body-parser';
-import ws from 'ws';
-import Redis from 'ioredis';
 
 // Route Controllers
 import UserController from './controllers/UserController.js';
@@ -15,17 +13,12 @@ import OnlineController from './controllers/OnlineController.js';
 import EventController from './controllers/EventController.js';
 import FileController from './controllers/FileController.js';
 import FeedbackController from './controllers/FeedbackController.js';
-// import IdsController from './controllers/IdsController.js';
+import IdsController from './controllers/IdsController.js';
 
 env.config();
 
 // Setup Express
 const app = express();
-// ws(app/*, null, {
-// 	wsOptions: {
-// 		clientTracking: true,
-// 	}
-// }*/);
 app.use(cookie());
 app.use(express.json());
 app.use(body.json({limit: '50mb'}));
@@ -42,14 +35,13 @@ app.use(cors({
 	credentials: true,
 }));
 
-// app.use((req, res, next) => {
-// 	console.log(req.headers.host);
-// 	res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-// 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-// 	res.setHeader('Access-Control-Allow-Credentials', true);
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 
 // Connect to MongoDB
 mongoose.set('toJSON', {virtuals: true});
@@ -58,94 +50,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex:
 const db = mongoose.connection;
 db.once('open', () => console.log('Successfully connected to MongoDB'));
 
-const redis = new Redis(process.env.REDIS_URI);
-
-// const IdsController = express.Router();
-
-// IdsController.ws('/', ws => {
-// 	console.log(ws);
-// 	sub.on('message', async (channel, message) => {
-// 		switch(channel) {
-// 			case "METAR:UPDATE":
-// 				ws.send('metar');
-// 				break;
-// 			case "ATIS:UPDATE":
-// 				ws.send('atis');
-// 				break;
-// 			case "PILOT:UPDATE":
-// 				ws.send(JSON.stringify(await redis.hgetall(`PILOT:${message}`)));
-// 				break;
-// 			default:
-// 				ws.send('idk');
-// 		}
-// 		// ws.send(`Channel: ${channel} - Message: ${message}`);
-// 	});
-
-// 	ws.on('close', () => {
-// 		sub.unsubscribe('METAR:UPDATE', 'ATIS:UPDATE', 'PILOT:UPDATE');
-// 	});
-// });
-
-
 app.use('/online', OnlineController);
 app.use('/user', UserController);
 app.use('/controller', ControllerController);
 app.use('/event', EventController);
 app.use('/file', FileController);
 app.use('/feedback', FeedbackController);
-// app.use('/ids', IdsController);
+app.use('/ids', IdsController);
 
-
-const expServer = app.listen('3000', () =>{
+app.listen('3000', () =>{
 	console.log('Listening on port 3000');
-});
-
-const wsserver = new ws.Server({noServer: true});
-
-expServer.on('upgrade', (req, soc, head) => {
-	wsserver.handleUpgrade(req, soc, head, soc => {
-		wsserver.emit('connection', soc, req);
-	});
-});
-
-wsserver.on('connection', (ws, req) => {
-	const sub = new Redis(process.env.REDIS_URI);
-	switch(req.url) {
-		case "/ids/aircraft": 
-			sub.subscribe('PILOT:UPDATE', 'PILOT:DELETE');
-			sub.on('message', async (channel, message) => {
-				if(channel === "PILOT:UPDATE") {
-					let res = await redis.hgetall(`PILOT:${message}`);
-					res.type = 'update';
-					ws.send(JSON.stringify(res));
-				}
-				if(channel === "PILOT:DELETE") {
-					ws.send(JSON.stringify({
-						type: 'delete',
-						callsign: message
-					}));
-				}
-			});
-			break;
-		default:
-			return;
-
-	}
-	// if(req.url === '/ids') {
-	// 	sub.on('message', async (channel, message) => {
-	// 		switch(channel) {
-	// 			case "METAR:UPDATE":
-	// 				// ws.send('metar');
-	// 				break;
-	// 			case "ATIS:UPDATE":
-	// 				// ws.send('atis');
-	// 				break;
-	// 			case "PILOT:UPDATE":
-	// 				ws.send(JSON.stringify(await redis.hgetall(`PILOT:${message}`)));
-	// 				break;
-	// 			default:
-	// 				ws.send('idk');
-	// 		}
-	// 	});
-	// }
 });
