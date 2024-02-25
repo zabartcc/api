e80a3f259136e61b146b2cc85553331973d20293
Cole D. Connelly <cdconn00@gmail.com>
2024-02-25 14:19:35 -0600

    Tweak training milestone model.

--- models/TrainingMilestone.js
+++ models/TrainingMilestone.js
@@ -3,8 +3,10 @@ import m from 'mongoose';
 const trainingMilestoneSchema = new m.Schema({
 	code: String,
 	name: String,
-	rating: Number,
-	certCode: String
+	rating: Number, // Legacy field, replaced by "availableAtRatings".
+	certCode: String, // Legacy field, replaced by "hiddenWhenControllerHasTierOne".
+	availableAtRatings: Array,
+	hiddenWhenControllerHasTierOne: Boolean
 });
 
 export default m.model('TrainingMilestone', trainingMilestoneSchema, 'trainingMilestones');
